09d260534a8ae7430a73a52eb2b1500f68178dbe
Mihail <mihailk.kolev@gmail.com>
2014-09-02 21:19:34 +0300

    created a new controller and replaced route

--- app/code/local/Fiuze/Dailysale/controllers/AccountController.php
+++ app/code/local/Fiuze/Dailysale/controllers/AccountController.php
@@ -11,14 +11,48 @@ require_once 'Mage/Customer/controllers/AccountController.php';
 class Fiuze_Dailysale_AccountController extends Mage_Customer_AccountController
 {
 
-    /**
-     * Existing user page
-     */
-    public function forgotPasswordAction()
+    public function preDispatch()
     {
+        // a brute-force protection here would be nice
+
+        //parent::preDispatch();
+
+        if (!$this->getRequest()->isDispatched()) {
+            return;
+        }
+
+        $action = $this->getRequest()->getActionName();
+        $openActions = array(
+            'create',
+            'login',
+            'logoutsuccess',
+            'forgotpassword',
+            'forgotpasswordpost',
+            'resetpassword',
+            'resetpasswordpost',
+            'confirm',
+            'confirmation',
+			'existinguser'
+        );
+        $pattern = '/^(' . implode('|', $openActions) . ')/i';
+
+        if (!preg_match($pattern, $action)) {
+            if (!$this->_getSession()->authenticate($this)) {
+                $this->setFlag('', 'no-dispatch', true);
+            }
+        } else {
+            $this->_getSession()->setNoReferer(true);
+        }
+    }
+
+    /**
+     * Existing user action
+     */
+    public function existingUserAction()
+    {   
         $this->loadLayout();
 
-        $this->getLayout()->getBlock('forgotPassword')->setEmailValue(
+        $this->getLayout()->getBlock('existingUser')->setEmailValue(
             $this->_getSession()->getForgottenEmail()
         );
         $this->_getSession()->unsForgottenEmail();
