fb1ecfacc96ea6cbcec0e1d9a76348cf67cc359f
jenkins <jenkins@dailysale.magemojo.com>
2014-11-14 03:12:09 +0000

    server changes into master

--- app/code/core/Mage/Core/Model/Email/Template/Filter.php
+++ app/code/core/Mage/Core/Model/Email/Template/Filter.php
@@ -354,7 +354,7 @@ class Mage_Core_Model_Email_Template_Filter extends Varien_Filter_Template
     {
         if (count($this->_templateVars)==0) {
             // If template preprocessing
-            return $construction[0];
+        //    return $construction[0];
         }
 
         $parts = explode('|', $construction[2], 2);
