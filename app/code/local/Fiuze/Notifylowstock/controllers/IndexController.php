b432f8d8133135f2b1c351dca697c9d241fd3e4c
Sergey Dorogavtsev <dorikzp@gmail.com>
2015-03-10 11:34:21 +0200

    162-notify code review

--- app/code/local/Fiuze/Notifylowstock/controllers/IndexController.php
+++ app/code/local/Fiuze/Notifylowstock/controllers/IndexController.php
@@ -3,7 +3,7 @@
 
 class Fiuze_Notifylowstock_IndexController extends Mage_Core_Controller_Front_Action
 {
-    public function IndexAction()
+    public function cronSendAction()
     {
         if(Mage::helper('fiuze_notifylowstock')->getModuleEnabled()){
             $cronModel = Mage::getModel('fiuze_notifylowstock/cron');
