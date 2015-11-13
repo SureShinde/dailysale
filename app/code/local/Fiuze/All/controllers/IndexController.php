4728c848a2782216054dc5062ce0b6f98fcf69c8
Alex <alex@www>
2015-11-13 15:55:21 +0200

    stop work

--- app/code/local/Fiuze/All/controllers/IndexController.php
+++ app/code/local/Fiuze/All/controllers/IndexController.php
@@ -12,6 +12,7 @@ class Fiuze_All_IndexController extends Mage_Core_Controller_Front_Action{
     public function indexAction(){
        echo'test_fuize_cron';
     //    Mage::getModel('track/observer')->notificationMail();
-        Mage::getModel('aftershiptrackcron/observer')->changeStatusShip('a');
+//        Mage::getModel('aftershiptrackcron/observer')->changeStatusShip('a');
+        Mage::getModel('udbatch/observer')->processStandard();
     }
 }
